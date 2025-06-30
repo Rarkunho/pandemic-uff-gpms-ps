1a88dab2e0b4a6142c08d4db0055ab80285a5d46
Italo <112668835+Rarkunho@users.noreply.github.com>
2025-06-30 20:34:54 -0300

    fix: compatibilidade

--- main.py
+++ main.py
@@ -1,37 +1,26 @@
 import pygame
-import platform
-import ctypes
+from game import Game
+from board import Board
 
-# Initialize Pygame
 pygame.init()
 
-# Screen dimensions
-if platform.system() == "Windows":
-    user32 = ctypes.windll.user32
-    WIDTH = user32.GetSystemMetrics(0)
-    HEIGHT = user32.GetSystemMetrics(1)
-else:
-    display_info = pygame.display.Info()
-    WIDTH = display_info.current_w
-    HEIGHT = display_info.current_h
+# resolução
+WIDTH, HEIGHT = 1200, 800
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("PANDEMIC UFF")
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-pygame.display.set_caption("Pandemic Board Game")
-
-# Colors
 WHITE = (255, 255, 255)
 
 def main():
-    """Main game loop"""
     clock = pygame.time.Clock()
     from board import WIDTH, HEIGHT
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
-    pygame.display.set_caption("Pandemic")
+    pygame.display.set_caption("PPANDEMIC UFF")
     game = Game()
     game.set_game_initial_state()
     board = Board(game)
-    game.start_game() 
-
+    game.board = board
+    
     running = True
     while running:
         for event in pygame.event.get():
